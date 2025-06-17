21d58d1b451dea2e141f0abe1fd103643fc76d7c
Rafael <braga3.coelho3@gmail.com>
2025-06-16 22:33:35 -0300

    Adição do teste da função na main
    3 linhas onde onde separa um vetor especifico para realizar a operação

--- main.go
+++ main.go
@@ -15,4 +15,7 @@ func main() {
 	primes := []int{11, 2, 2, 2, 5, 3, 3, 11, 5, 11, 13, 5, 2, 3, 11}
 	// executar_estrategia(&primes, removeDuplicates)
 	fmt.Println("Remoção de duplicatas: ",primes)
+	primes2 := []int{11, 2, 2, 2, 5, 3, 3, 11, 5, 11, 13, 5, 2, 3, 11}
+	executar_estrategia(&primes, StrategyFuncs.FiltraPares)
+	fmt.Println("Pares Filtrados: ",primes2)
 }
